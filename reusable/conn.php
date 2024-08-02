1b76020d016a5297bb232d5c04169c8056b1d23a
Irina Balanel <irina.cowork@gmail.com>
2024-08-02 14:47:53 -0400

    Update conn.php
    Removed connection string to infinityfree

--- reusable/conn.php
+++ reusable/conn.php
@@ -1,9 +1,6 @@
 <?php
   // connection string for local database in MAMP
   $connect = mysqli_connect('localhost', 'root', 'YOURPASSWORD', 'publicart');
-
-  // connection string for the database on infinityfree. To connect to this database, the code that queries tables needs to be adjusted from uppercase to lowercase names of tables
-  // $connect = mysqli_connect('sql208.infinityfree.com', 'if0_37023836', '4WtMjCeBkmRQrj', 'if0_37023836_publicart');
   if (!$connect) {
     die("Connection Failed: " . mysqli_connect_error());
   }
