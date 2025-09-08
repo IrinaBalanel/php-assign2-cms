ce8e5e569dffd91ab3064ecf74eb57e28bb08f44
Irina Balanel <irina.cowork@gmail.com>
2025-09-08 08:18:04 -0400

    updated password

--- reusable/conn.php
+++ reusable/conn.php
@@ -1,6 +1,6 @@
 <?php
   // connection string for local database in MAMP
-  $connect = mysqli_connect('localhost', 'root', 'YOURPASSWORD', 'publicart');
+  $connect = mysqli_connect('localhost', 'root', 'root', 'publicart');
   if (!$connect) {
     die("Connection Failed: " . mysqli_connect_error());
   }
